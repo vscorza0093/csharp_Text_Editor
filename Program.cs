f981f1ab87b5282dcd097d70c7c7cdc910204a76
viniciusscorza <vinicius.scorza@hotmail.com>
2023-03-10 04:00:25 -0300

    Updating project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Text Editor\n");
+            Console.WriteLine("What do you wish?");
+            Console.WriteLine("1 - Open File");
+            Console.WriteLine("2 - New file");
+            Console.WriteLine("Any other key to quit application");
+
+            char option = Console.ReadLine()[0];
+
+            switch (option)
+            {
+                case '1': OpenFile(); break;
+                case '2': EditFile(); break;
+                default:  
+                    Console.WriteLine("Thanks for using");
+                    Thread.Sleep(1000); Console.Clear();
+                    System.Environment.Exit(1); 
+                    break;
+            }
+        }
+
+        static void OpenFile()
+        {
+            Console.Clear();
+            Menu();
+        }
+
+        static void EditFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Type your text below and ESC to quit\n");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write($"{text}");
+            Thread.Sleep(1000);
+            Console.WriteLine("Enter any key to continue");
+
+            switch(Console.ReadLine())
+            {
+                default: Menu(); break;
+            }
+        }
+
+        static void SaveFile(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Select a path to save the file");
+            var path = Console.ReadLine();
+
+            
+            Menu();
+        }
+    }
+}
